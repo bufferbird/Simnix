61f6f3cbc0e45e348d82fd7276a0f70178e16245
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-04-23 19:34:24 +0200

    Update init.c

--- src/init/init.c
+++ src/init/init.c
@@ -2,21 +2,21 @@
 
 
 void uart_init() {
-    *UART0_CR = 0; // UART0 kurz ausschalten zum Konfigurieren
+    *UART0_CR = 0; 
 
-    // GPIO 14 & 15 auf UART0 umstellen (Alt Function 0)
-    *GPFSEL1 &= ~((7 << 12) | (7 << 15)); // Bits löschen
-    *GPFSEL1 |= (4 << 12) | (4 << 15);    // Bits auf 100 setzen (Alt0)
+  
+    *GPFSEL1 &= ~((7 << 12) | (7 << 15));
+    *GPFSEL1 |= (4 << 12) | (4 << 15);    
 
-    *GPPUD = 0; // Pull-up/down deaktivieren
+    *GPPUD = 0; 
     for(int i=0; i<150; i++) __asm__("nop");
     *GPPUDCLK0 = (1 << 14) | (1 << 15);
     for(int i=0; i<150; i++) __asm__("nop");
     *GPPUDCLK0 = 0;
 
-    *UART0_ICR = 0x7FF; // Alle Interrupts löschen
-    *UART0_IBRD = 26;   // Baudrate auf 115200 (bei 48MHz)
+    *UART0_ICR = 0x7FF; // delete all interrupts
+    *UART0_IBRD = 26;   
     *UART0_FBR = 3;
-    *UART0_LCRH = (3 << 5); // 8-bit Modus, kein Parity
-    *UART0_CR = 0x301;  // UART wieder an (RX, TX, Enable)
+    *UART0_LCRH = (3 << 5); 
+    *UART0_CR = 0x301;  
 }
