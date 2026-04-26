1766631a887e48e98b0b374eade81ac86f3168ea
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-04-26 14:26:05 +0200

    Remove vga_init function from core.c
    Removed the vga_init function to streamline initialization process.

--- core.c
+++ core.c
@@ -40,56 +40,8 @@ static void __initscreen__(){
 
 }
 
-static void vga_init(void){
-    /* Yes. we have to do this. */
-    mailbox[0] = 35 * 4;  
-    mailbox[1] = 0;        
-
-    mailbox[2] = 0x00048003; 
-    mailbox[3] = 8;
-    mailbox[4] = 8;
-    mailbox[5] = 1024; // width
-    mailbox[6] = 768;  // height
-
-    mailbox[7] = 0x00048004;
-    mailbox[8] = 8;
-    mailbox[9] = 8;
-    mailbox[10] = 1024;
-    mailbox[11] = 768;
-
-    mailbox[12] = 0x00048005;
-    mailbox[13] = 4;
-    mailbox[14] = 4;
-    mailbox[15] = 32;
-
-    mailbox[16] = 0x00040001;
-    mailbox[17] = 8;
-    mailbox[18] = 8;
-    mailbox[19] = 4096;
-    mailbox[20] = 0; 
-
-    mailbox[21] = 0; 
-
-    // Send
-    while (*(volatile uint32_t*)MAILBOX_STATUS & 0x80000000);
-    *(volatile uint32_t*)MAILBOX_WRITE = ((uint32_t)(uintptr_t)mailbox & ~0xF) | 8;
-
-    // Wait
-    while (1) {
-        while (*(volatile uint32_t*)MAILBOX_STATUS & 0x40000000);
-        uint32_t response = *(volatile uint32_t*)MAILBOX_READ;
-        if ((response & 0xF) == 8) break;
-    }
 
 
-    fb_ptr = (uint32_t*)(uintptr_t)(mailbox[20] & 0x3FFFFFFF);
-}
-
-/*
- * Sooo, now we have the Pointer, and we can do funny things with it. 
- * But let's init UART first, and then wie can use the beautyful 
- * __initscreen__ to use fbr_ptr thee very first time. 
-*/
 
 void k_main(void){
   uart_init(); 
