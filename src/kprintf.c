eaedcfdb2fb86a8a7bfaa6f608e2763a2bbfc6fc
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-14 16:47:00 +0200

    Update kprintf.c

--- src/kprintf.c
+++ src/kprintf.c
@@ -144,3 +144,14 @@ void kprintf(const char* fmt, ...) {
     }
     va_end(args);
 }
+
+
+
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
