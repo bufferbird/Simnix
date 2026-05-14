5260330b07dde4c2ffbd2a95e60be92bd117923b
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-14 16:31:38 +0200

    Update kprintf.h

--- src/kprintf.h
+++ src/kprintf.h
@@ -4,7 +4,7 @@
 void kputc(char c);
 void kprint(const char* s);
 void kprintf(const char* format, ...); 
-
+void k_getc(void); 
 void kprint_hex(uint32_t d);
 void kset_cursor(uint32_t x, uint32_t y);
 void kclear_screen(uint32_t color);
