a6064226557ef73c55913db6500f04da8141572f
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-14 13:35:31 +0200

    Delete src directory

--- src/init/init.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef INIT_H
-#define INIT_H
-
-
-void vga_init(); 
-void uart_init(void); 
-
-#endif
--- src/init/main.c
+++
@@ -1,9 +0,0 @@
-#include "src/init/init.h"
-#include "src/hardware/hardwarepi3.h"
-#include "src/stdDatatypes.h"
-
-__init_main__(){
-  uart_init();
-  vga_init();
-}
-
