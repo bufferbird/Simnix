a81a175a8a8bfe212c43438df5e6c14fc7ad2ada
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-15 15:31:51 +0200

    Fix duplicate fb_ptr declaration and align mailbox
    Removed duplicate declaration of fb_ptr and adjusted alignment.

--- core.c
+++ core.c
@@ -6,6 +6,7 @@
 #include <stdalign.h>
 #include <stddef.h>
 
+uint32_t* fb_ptr = 0;
 #define VERSION 1.2
 
 extern uint64_t get_main_id(void);
@@ -43,7 +44,7 @@ void draw_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t
 #define TAG_END         0x00000000
 
 static alignas(16) uint32_t mailbox[35];
-uint32_t* fb_ptr = 0;
+
 
 static void vga_init() {
     mailbox[0] = 35 * sizeof(uint32_t); 
