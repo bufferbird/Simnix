8a4774e36a5003c69debb38f4bacbcebf3f4d8e8
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-15 12:49:55 +0200

    Update input.c

--- src/input.c
+++ src/input.c
@@ -1,7 +1,7 @@
 #include "src/kprintf.h"
 #include "src/input.h"
 
-void k_input(char prompt[50], char* buffer, int max_size) {
+void k_input(const char* prompt, char* buffer, int max_size) {
     uint32_t index = 0; 
     kprintf("%s", prompt); 
     while (index < (max_size - 1)) {
