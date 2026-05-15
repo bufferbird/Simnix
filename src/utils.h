7d462a93861e3cc22adf5f0e7872ba4aa7af33d3
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-15 12:37:05 +0200

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+typedef unsigned long size_t;
+
 int strcmp(const char *s1, const char *s2); 
 size_t strlen(const char* str); 
 char* strcpy(char* dest, const char* src); 
