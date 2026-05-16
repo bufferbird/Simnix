e91f69353cec0f2bdc7ced973f9aa94545cceb6c
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-05-16 17:44:52 +0200

    Update kprintf message for GPFSEL configuration

--- src/init/init.c
+++ src/init/init.c
@@ -9,7 +9,7 @@ void uart_init() {
 
     *GPFSEL1 &= ~((7 << 12) | (7 << 15));
     *GPFSEL1 |= (4 << 12) | (4 << 15);    
-    kprintf("*GPFSEL &= ~((7 << 12) | (7 << 15))"); 
+    kprintf("[ OK ] *GPFSEL &= ~((7 << 12) | (7 << 15))"); 
 
     *GPPUD = 0; 
     for(int i=0; i<150; i++) __asm__("nop");
